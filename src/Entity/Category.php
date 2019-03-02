5fb7be6d2026fee5c4263191e54ece30818599d0
Kunal Kharidia <kharidiakunal@gmail.com>
2019-03-02 15:32:34 -0500

    Added new packages api platorm, jwt authentication package

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,9 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection; 
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
+ * @ApiResource()
  */
 class Category
 {
@@ -20,6 +24,28 @@ class Category
      * @ORM\Column(type="string", length=255)
      */
     private $name;
+
+    /**
+     * @var ArrayCollection
+     * @ORM\OneToMany(targetEntity="App\Entity\Product", mappedBy="category")
+     */    
+    private $products;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modified_at;        
+
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -37,4 +63,40 @@ class Category
 
         return $this;
     }
+
+    /**
+     * @return Collection
+     * 
+     */
+
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }    
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeInterface
+    {
+        return $this->modified_at;
+    }
+
+    public function setModifiedAt(\DateTimeInterface $modified_at): self
+    {
+        $this->modified_at = $modified_at;
+
+        return $this;
+    }    
+
+
 }
