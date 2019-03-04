6f7965661c21f42b0e09e196c522b29b52a05c0f
Kunal Kharidia <kharidiakunal@gmail.com>
2019-03-03 22:06:33 -0500

    Made changes in User entity to validate email and username for uniqueness, Added Group annotation to read selected fields in Api listing

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,22 +4,34 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *      normalizationContext={
+ *          "groups"={"read"}
+ *      }
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity("username")
+ * @UniqueEntity("email")
+ 
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read"})
      */
     private $username;
 
@@ -30,23 +42,32 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read"}) 
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read"})
      */
     private $email;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"read"})
      */
     private $created_at;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"read"})
      */
     private $modified_at;
+
+    public function __construct() {
+        $this->setCreatedAt(new \DateTime());
+        $this->setModifiedAt(new \DateTime());
+    }    
 
     public function getId(): ?int
     {
@@ -118,10 +139,29 @@ class User
         return $this->modified_at;
     }
 
+    /**
+    * @ORM\PreUpdate
+    */        
+
     public function setModifiedAt(\DateTimeInterface $modified_at): self
     {
         $this->modified_at = $modified_at;
 
         return $this;
     }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+    }    
+
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }    
 }
